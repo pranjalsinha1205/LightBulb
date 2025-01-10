bcd0c8fabe99e1488f16dc0c1323d00c0dc109d3
Pranjal Sinha <pranjal01sinha@gmail.com>
2025-01-10 14:09:34 +0530

    Changed from one button to two buttons

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
+// let bulb = document.getElementById("bulb");
+
+// function onBulb(){
+//     // on the bulb
+//     bulb.innerHTML = ``;
+//     bulb.innerHTML = `
+//         <button onclick=onBulb() id="on">Click to <strong>ON</strong> the bulb</button>
+//         <img src="pic_bulbon.gif" alt="Bulb is on" title="Bulb is on">
+//         <button onclick=offBulb() id="off">Click to <strong>OFF</strong> the bulb</button>
+//     `
+// }
+
+// function offBulb(){
+//     // off the bulb
+//     bulb.innerHTML = ``;
+//     bulb.innerHTML = `
+//         <button onclick=onBulb() id="on">Click to <strong>ON</strong> the bulb</button>
+//         <img src="pic_bulboff.gif" alt="Bulb is off" title="Bulb is off">
+//         <button onclick=offBulb() id="off">Click to <strong>OFF</strong> the bulb</button>
+//     `}
+
+// let actionButton = document.getElementById("actionButton");
+// let bulbDiv = document.getElementById("bulb");
+
+// function bulbAction() {
+//     if (actionButton.innerHTML === "ON") {
+//         // Turn the bulb ON
+//         bulbDiv.innerHTML = `
+//             <img src="pic_bulbon.gif" alt="Bulb is on" title="Bulb is on">
+//             <button onclick="bulbAction()" id="actionButton">OFF</button>
+//         `;
+//         actionButton.innerHTML = "OFF";
+//     } else if (actionButton.innerHTML === "OFF"){
+//         // Turn the bulb OFF
+//         bulbDiv.innerHTML = `
+//             <img src="pic_bulboff.gif" alt="Bulb is off" title="Bulb is off">
+//             <button onclick="bulbAction()" id="actionButton">ON</button>
+//         `
+//         actionButton.innerHTML = "ON";
+//         ;
+//     }
+// }
+
+let actionButton = document.getElementById("actionButton");
 let bulb = document.getElementById("bulb");
-let on = document.getElementById("on");
-let off = document.getElementById("off");
 
-function onBulb(){
-    // on the bulb
-    bulb.innerHTML = ``;
-    bulb.innerHTML = `
-        <button onclick=onBulb() id="on">Click to <strong>ON</strong> the bulb</button>
-        <img src="pic_bulbon.gif" alt="Bulb is on" title="Bulb is on">
-        <button onclick=offBulb() id="off">Click to <strong>OFF</strong> the bulb</button>
-    `
+function bulbAction(){
+    if (actionButton.innerHTML === "ON"){
+        bulb.innerHTML = `<img src="pic_bulbon.gif" alt="Bulb is on" title="Bulb is on">`;
+        actionButton.innerHTML = "OFF";
+    }
+    else if (actionButton.innerHTML === "OFF"){
+        bulb.innerHTML = `<img src="pic_bulboff.gif" alt="Bulb is off" title="Bulb is off">`;
+        actionButton.innerHTML = "ON";
+    }
 }
-
-function offBulb(){
-    // off the bulb
-    bulb.innerHTML = ``;
-    bulb.innerHTML = `
-        <button onclick=onBulb() id="on">Click to <strong>ON</strong> the bulb</button>
-        <img src="pic_bulboff.gif" alt="Bulb is off" title="Bulb is off">
-        <button onclick=offBulb() id="off">Click to <strong>OFF</strong> the bulb</button>
-    `}
